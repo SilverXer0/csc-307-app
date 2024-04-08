0e936c45c12363a37f9068530a7e95862bb57852
Sharan Krishna <87695707+SilverXer0@users.noreply.github.com>
2024-04-07 19:43:32 -0700

    completed assignemnt

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -3,22 +3,25 @@ import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-      function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-          return i !== index;
-        });
-        setCharacters(updated);
-      }
-
-    return (
-      <div className="container">
-        <Table 
-        characterData={characters}
-        removeCharacter = {removeOneCharacter}/>
-        < Form />
-      </div>
-    );
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
   }
-  export default MyApp;
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList}/>
+    </div>
+  );
+}
+export default MyApp;
