4227d87d6925e06a14bc5ff5cbc731e1d354e9fa
Sharan Krishna <87695707+SilverXer0@users.noreply.github.com>
2024-04-10 18:10:24 -0700

    post and fetch working

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -32,18 +32,34 @@ const users = {
 
 const findUserByName = (name) => {
     return users["users_list"].filter(
-      (user) => user["name"] === name
+        (user) => user["name"] === name
     );
-  };
+};
 
 const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+    users["users_list"].find((user) => user["id"] === id);
+  
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
   
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
@@ -55,7 +71,17 @@ app.get("/users/:id", (req, res) => {
     }
   });
   
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+fetch(`http://localhost:${port}/users`)
+
+
+
